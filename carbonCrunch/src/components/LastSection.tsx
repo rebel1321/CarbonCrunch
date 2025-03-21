a17e18ab5c650a46739245fe4591a5a3da08d498
Satyam <www.satyam200413@gmail.com>
2025-03-21 20:33:47 +0530

    This is an assignment project given by Carbon Crunch in which i use React + tyu+pescript and talwind as a tech stack

--- carbonCrunch/src/components/LastSection.tsx
+++ carbonCrunch/src/components/LastSection.tsx
@@ -21,18 +21,18 @@ const LastSection = () => {
         </div>
 
         <div className="bg-gray-800 text-white w-72 p-4 rounded-lg relative z-30 -mt-8 -ml-10">
-          <h1 className="font-bold text-3xl text-left">3X</h1>
-          <p className="text-xs">ESG high performers deliver a higher total shareholder return</p>
+          <h1 className="font-bold text-3xl text-left">98%</h1>
+          <p className="text-xs">of CEOs agree sustainability is their responsibility</p>
         </div>
 
         <div className="bg-gray-800 text-white w-72 p-4 rounded-lg relative z-20 -mt-8 ml-55">
-          <h1 className="font-bold text-3xl text-right">3X</h1>
-          <p className="text-xs">ESG high performers deliver a higher total shareholder return</p>
+          <h1 className="font-bold text-3xl text-right">18%</h1>
+          <p className="text-xs">of companies are cutting emissions fast enough to reach net zero by 2050</p>
         </div>
 
         <div className="bg-white text-green-500 w-72 p-4 rounded-lg relative z-10 -mt-8 -ml-15">
-          <h1 className="font-bold text-3xl text-left">3X</h1>
-          <p className="text-xs">ESG high performers deliver a higher total shareholder return</p>
+          <h1 className="font-bold text-3xl text-left">37%</h1>
+          <p className="text-xs">of the world's largest companies have a public net zero target. Nearly all are off track</p>
         </div>
       </div>
     </div>
